63f5e5849a0d3d246f725d59fb323354abcaf404
softwareByAndi <andrew.david.ahlstrom@gmail.com>
2022-11-16 20:33:58 -0700

    cleaning

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
 #include "engine/vk_engine.h"
-#include <iostream>
 #include "engine/compute/device/device.hpp"
 #include "pretty_io.hpp"
+
+#include <iostream>
 
 int main(int argc, char *argv[]) {
   io::printColorTest();
   {
-    walrus::VulkanEngine engine{walrus::ALL};
+    walrus::VulkanEngine engine{walrus::DeviceTask::ALL};
     engine.run();
   }
   return 0;
